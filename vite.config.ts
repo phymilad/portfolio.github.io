e134bf5b81045e01092f6abd789c76328c97b129
phymilad <phy.milad@gmail.com>
2025-01-14 08:44:13 +0330

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio.github.io/",
+  base: '/portfolio.github.io/', // Replace 'portfolio.github.io' with your repo name
   plugins: [react()],
-})
+});
